192628f5d31ea8662a59a7955d96eb5167eb621a
WEIYANG\User <weiyang.wyc@gmail.com>
2022-11-18 15:20:19 +0800

    a

--- GogoTravel/salesreport.php
+++ GogoTravel/salesreport.php
@@ -26,40 +26,40 @@
   <!--center-->
   <div class="col-sm-8">
     <div class="row">
-      <div class="col-xs-12">
-        <h3 style="padding-left: 100px;">How to get sales report from database between two dates in php and MySQL</h3>
-		<hr >
+        <div class="col-xs-12">
+        <h3 style="padding-left: 100px;">Sales Report</h3>
+		<hr>
 		<form name="bwdatesdata" action="" method="post" action="">
-  <table width="100%" height="117"  border="0">
-<tr>
-    <th width="27%" height="63" scope="row">From Date :</th>
-    <td width="73%">
-<input type="date" name="fdate" class="form-control" id="fdate">
+        <table width="100%" height="117"  border="0">
+        <tr>
+        <th width="27%" height="63" scope="row">From Date :</th>
+        <td width="73%">
+        <input type="date" name="fdate" class="form-control" id="fdate">
     	</td>
-  </tr>
+        </tr>
  
-  <tr>
-    <th width="27%" height="63" scope="row">To Date :</th>
-    <td width="73%">
+        <tr>
+        <th width="27%" height="63" scope="row">To Date :</th>
+        <td width="73%">
     	<input type="date" name="tdate" class="form-control" id="tdate"></td>
-  </tr>
-  <tr>
-    <th width="27%" height="63" scope="row">Request Type :</th>
-    <td width="73%">
-         <input type="radio" name="requesttype" value="mtwise" checked="true">Month wise
-          <input type="radio" name="requesttype" value="yrwise">Year wise</td>
-  </tr>
-<tr>
-    <th width="27%" height="63" scope="row"></th>
-    <td width="73%">
-    	<button class="btn-primary btn" type="submit" name="submit">Submit</button>
-  </tr>
- 
-</table>
-     </form>
- 
-      </div>
+        </tr>
+        <tr>
+        <th width="27%" height="63" scope="row">Request Type :</th>
+        <td width="73%">
+        <input type="radio" name="requesttype" value="mtwise" checked="true">Month wise
+        <input type="radio" name="requesttype" value="yrwise">Year wise</td>
+        </tr>
+        <tr>
+        <th width="27%" height="63" scope="row"></th>
+        <td width="73%">
+    	<button class="btn-primary btn" type="submit" name="submit">Check</button>
+        </tr>
+        </table>
+        </form>
+        </div>
     </div>
+  </div>
+</div>
 </body>
  
 </html>>
