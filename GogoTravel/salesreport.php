2cb907d8225b0f5404532348337bbdf3a378414c
WEIYANG\User <weiyang.wyc@gmail.com>
2022-11-18 14:49:16 +0800

    a

--- GogoTravel/salesreport.php
+++ GogoTravel/salesreport.php
@@ -9,7 +9,7 @@
 <head lang="en">  
     <meta charset="UTF-8">  
     <link type="text/css" rel="stylesheet" href="bootstrap-3.2.0-dist\css\bootstrap.css">
-    <title>View Staff</title>  
+    <title>View Sales Report</title>  
 </head>
 <style>  
     .login-panel {  
@@ -20,5 +20,11 @@
      }  
 </style>  
   
-<body> 
+<body>
+
+<div class="table-scrol">  
+    <h1 align="center"><br>Staff Lists</h1>
+
+</div>
+</body>
 </html>
