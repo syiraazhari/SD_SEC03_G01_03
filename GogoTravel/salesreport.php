bee333be9ff84457dd42e573b3ce189fd8d92099
WEIYANG\User <weiyang.wyc@gmail.com>
2022-11-18 14:54:07 +0800

    aa

--- GogoTravel/salesreport.php
+++ GogoTravel/salesreport.php
@@ -23,8 +23,37 @@
 <body>
 
 <div class="table-scrol">  
-    <h1 align="center"><br>Staff Lists</h1>
+    <h1 align="center"><br>Sales Report</h1>
 
+    <form name="bwdatesdata" action="" method="post" action="">
+        <table width="100%" height="117"  border="0">
+        <tr>
+        <th width="27%" height="63" scope="row">From Date :</th>
+        <td width="73%">
+        <input type="date" name="fdate" class="form-control" id="fdate">
+    	</td>
+        </tr>
+ 
+        <tr>
+        <th width="27%" height="63" scope="row">To Date :</th>
+        <td width="73%">
+    	<input type="date" name="tdate" class="form-control" id="tdate"></td>
+        </tr>
+        
+        <tr>
+        <th width="27%" height="63" scope="row">Request Type :</th>
+        <td width="73%">
+        <input type="radio" name="requesttype" value="mtwise" checked="true">Month wise
+        <input type="radio" name="requesttype" value="yrwise">Year wise</td>
+        </tr>
+
+        <tr>
+        <th width="27%" height="63" scope="row"></th>
+        <td width="73%">
+    	<button class="btn-primary btn" type="submit" name="submit">Check</button>
+        </tr>
+        </table>
+     </form>
 </div>
 </body>
 </html>
