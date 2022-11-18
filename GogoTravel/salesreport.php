3746f7714c9a93a364bd89c155b6286445f87500
WEIYANG\User <weiyang.wyc@gmail.com>
2022-11-18 15:16:19 +0800

    a

--- GogoTravel/salesreport.php
+++ GogoTravel/salesreport.php
@@ -22,52 +22,44 @@
   
 <body>
 
-<div class="table-scrol">  
-    <h1 align="center"><br>Sales Report</h1>
-
-        <table class="table table-bordered table-hover table-striped" style="table-layout: fixed">  
-        <thead>
-        <tr>  
-            <th style="text-align: center">From Date :</th>
-            <td><input type="date" name="fdate" class="form-control" id="fdate"></td>
-
-            <th style="text-align: center">To Date :</th>
-            <td><input type="date" name="tdate" class="form-control" id="tdate"></td>
-
-            <th style="text-align: center">Request Type :</th>
-            <td><input type="radio" name="requesttype" value="mtwise" checked="true">Month wise
-            <input type="radio" name="requesttype" value="yrwise">Year wise</td>
-            <td><button class="btn-primary btn" type="submit" name="submit">Check</button></td>
-        </tr>  
-        </thead>
-
-        <?php  
-          
-        include 'config.php';
-        $sql = "SELECT * FROM user WHERE usertype = 'staff'";
-        $result = mysqli_query($conn, $sql);
-
-        while($row=mysqli_fetch_array($result))
-        {  
-            $user_id=$row[0];  
-            $user_fullname=$row[2];
-            $user_username=$row[1];  
-            $user_password=$row[3];  
-            $user_email=$row[4];  
-        
-            $show = "<p> Username : $user_username </p>" . "<p> Email : $user_email </p>" . "<p> Password : $user_password </p>" 
-        ?>
-    
-        <tr>
-            <td style="text-align: center"><br><?php echo $user_id;  ?></td>  
-            <td style="text-align: center"><br><?php echo $user_fullname;  ?></td>
-            <td ><br><?php echo $show;  ?></td>   
-            <td style="text-align: center"><br><a href="delete-user.php?del=<?php echo $user_id ?>"><button class="btn btn-danger">Delete</button></a>
-                <a href="admin-edit-staff.php?update_staff=<?php echo $user_id ?>"><button class="btn btn-warning">Edit</button></a></td> 
-    <?php } ?>
-    </table>
+<div class="container-fluid">
+  <!--center-->
+  <div class="col-sm-8">
+    <div class="row">
+      <div class="col-xs-12">
+        <h3 style="padding-left: 100px;">How to get sales report from database between two dates in php and MySQL</h3>
+		<hr >
+		<form name="bwdatesdata" action="" method="post" action="">
+  <table width="100%" height="117"  border="0">
+<tr>
+    <th width="27%" height="63" scope="row">From Date :</th>
+    <td width="73%">
+<input type="date" name="fdate" class="form-control" id="fdate">
+    	</td>
+  </tr>
+ 
+  <tr>
+    <th width="27%" height="63" scope="row">To Date :</th>
+    <td width="73%">
+    	<input type="date" name="tdate" class="form-control" id="tdate"></td>
+  </tr>
+  <tr>
+    <th width="27%" height="63" scope="row">Request Type :</th>
+    <td width="73%">
+         <input type="radio" name="requesttype" value="mtwise" checked="true">Month wise
+          <input type="radio" name="requesttype" value="yrwise">Year wise</td>
+  </tr>
+<tr>
+    <th width="27%" height="63" scope="row"></th>
+    <td width="73%">
+    	<button class="btn-primary btn" type="submit" name="submit">Submit</button>
+  </tr>
+ 
+</table>
+     </form>
+ 
+      </div>
     </div>
-</div>
 </body>
  
 </html>>
