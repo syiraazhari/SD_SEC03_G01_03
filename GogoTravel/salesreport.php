aa89c8250198bdce1c46aa1bd01ec13e87572edb
WEIYANG\User <weiyang.wyc@gmail.com>
2022-11-18 15:34:39 +0800

    a

--- GogoTravel/salesreport.php
+++ GogoTravel/salesreport.php
@@ -21,45 +21,18 @@
 </style>  
   
 <body>
+    <div class="table-scrol">
+        <h1 align="center"><br>Sales Report</h1>
 
-<div class="container-fluid">
-  <!--center-->
-  <div class="col-sm-8">
-    <div class="row">
-        <div class="col-xs-12">
-        <h3>Sales Report</h3>
-		<hr>
-		<form name="bwdatesdata" action="" method="post" action="">
-        <table width="100%" height="117"  border="0">
-        <tr>
-        <th width="27%" height="63" scope="row">From Date :</th>
-        <td width="73%">
-        <input type="date" name="fdate" class="form-control" id="fdate">
-    	</td>
-        </tr>
- 
-        <tr>
-        <th width="27%" height="63" scope="row">To Date :</th>
-        <td width="73%">
-    	<input type="date" name="tdate" class="form-control" id="tdate"></td>
-        </tr>
-        <tr>
-        <th width="27%" height="63" scope="row">Request Type :</th>
-        <td width="73%">
-        <input type="radio" name="requesttype" value="mtwise" checked="true">Month wise
-        <input type="radio" name="requesttype" value="yrwise">Year wise</td>
-        </tr>
-        <tr>
-        <th width="27%" height="63" scope="row"></th>
-        <td width="73%">
-    	<button class="btn-primary btn" type="submit" name="submit">Check</button>
-        </tr>
-        </table>
-        </form>
-        </div>
-    </div>
-  </div>
-</div>
+<div class="container col-lg-6">
+    <form name="bwdatesdata" action="" method="post" action=""> 
+        <table class="table table-bordered table-hover table-striped" style="table-layout: fixed">
+            <tr>
+                <th scope="row">From Date :</th>
+                <td width="73%">
+                    <input type="date" name="fdate" class="form-control" id="fdate">
+    	        </td>
+            </tr>
 </body>
  
 </html>>
